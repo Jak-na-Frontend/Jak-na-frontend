b68b1242dfc672d1a68bcaa0a9b1e6886da3254c
Iva Kubickova <ivakubickova55@gmail.com>
2021-06-05 15:16:44 +0200

    pole HTML doplneni

--- src/components/Quiz/questions.js
+++ src/components/Quiz/questions.js
@@ -220,5 +220,343 @@ export default {
       ok: '',
       link: 'http://htmlguru.cz/css-selektor-trida.html',
     },
+    {
+      id: '21',
+      text: 'Kdy se používá značka <link>?',
+      source: [
+        { text: 'Při propojování souborů se styly, JavaScriptu a ikon pro mobilní aplikace' },
+        { text: 'Při propojování souborů se styly, faviconů a assets' },
+        { text: 'Při propojování souborů se styly a faviconů' },
+        { text: 'Při propojování souborů se styly, externích adres URL a faviconů' },
+      ],
+      ok: '',
+      link: 'https://www.w3schools.com/tags/tag_link.asp',
+    },
+    {
+      id: '22',
+      text: 'K jakým značkám se pojí atribut value?',
+      source: [
+        { text: '<button><input><form>' },
+        { text: '<input><label><meter>' },
+        { text: '<input><option><textarea>' },
+        { text: '<li><input><option>' },
+      ],
+      ok: '',
+      link: 'https://www.w3schools.com/tags/att_input_value.asp',
+    },
+    {
+      id: '23',
+      text: 'K jakým značkám se pojí atribut value?',
+      source: [
+        { text: '<button><input><form>' },
+        { text: '<input><label><meter>' },
+        { text: '<input><option><textarea>' },
+        { text: '<li><input><option>' },
+      ],
+      ok: '',
+      link: 'https://www.w3schools.com/tags/att_input_value.asp',
+    },
+    // {
+    //   id: '24',
+    //   text: 'Doplňte do mezer 
+    //     <address ______ _____>
+    //     <span itemprop="streetAddress">6410 Via Real</span><br />
+    //     <span itemprop="addressLocality">Carpinteria</span>,
+    //     <span itemprop="addressRegion">CA</span>
+    //     <span itemprop="addressCode">93013</span>
+    //   </address>',
+    //   source: [
+    //     { text: 'itemscope itemtype="http://schema.org/PostalAddress"' },
+    //     { text: 'itemsref="http://schema.org/PostalAddress" itemid="address"' },
+    //     { text: 'itemscope itemref="http://schema.org/PostalAddress"' },
+    //     { text: 'itemid="address" itemtype="http://schema.org/PostalAddress"' },
+    //   ],
+    //   ok: '',
+    //   link: 'https://www.tutorialspoint.com/html5/html5_microdata.htm',
+    // },
+    {
+      id: '25',
+      text: 'Element aside se používá:',
+      source: [
+        { text: 'pro cokoli, co nepatří k hlavnímu tématu daného bloku' },
+        { text: 'pro cokoli, co chcete posunout na stranu ať už je to sidebar nebo obrázekobtékaný textem' },
+        { text: 'pro cokoli v závorkách' },
+        { text: 'pro cokoli v sidebaru' },
+      ],
+      ok: '',
+      link: 'https://www.w3schools.com/tags/tag_aside.asp',
+    },
+    {
+      id: '26',
+      text: 'Se kterými značkami se pojí prvek source?',
+      source: [
+        { text: '<svg>, <picture>, <audio>, and <video>' },
+        { text: '<picture>, <audio>, and <video>' },
+        { text: 'Protože je zaměnitelný s atributem src <source>, takže pro elementy, které používají src, můžeme použít <source>' },
+        { text: '<audio> and <video>' },
+      ],
+      ok: '',
+      link: 'https://www.w3schools.com/tags/tag_source.asp',
+    },
+    {
+      id: '27',
+      text: 'Který atribut se nepoužívá pro <input>?',
+      source: [
+        { text: '<input type = "color">' },
+        { text: '<input type = "tel">' },
+        { text: '<input type = "week">' },
+        { text: '<input type = "num">' },
+      ],
+      ok: '',
+      link: 'https://www.w3schools.com/tags/tag_input.asp',
+    },
+    {
+      id: '28',
+      text: 'Který HTML kód po kliknutí telefonuje na uvedené číslo?',
+      source: [
+        { text: '<a href="tel: 802-555-1212">Call me</a>' },
+        { text: '<a href="phone">802-555-1212</a>' },
+        { text: '<a href="tel">802-555-1212</a>' },
+        { text: '<a href="phone: 802-555-1212">Call me</a>' },
+      ],
+      ok: '',
+      link: 'https://developers.google.com/web/fundamentals/native-hardware/click-to-call',
+    },
+    {
+      id: '29',
+      text: 'Defer v kódu <script defer src = "myscript.js"> </script>?',
+      source: [
+        { text: 'Stahuje skript ze serveru.' },
+        { text: 'Spouští skript po dokončení HTML analýzy („parsování“)' },
+        { text: 'Spouští skript jakmile je to možné' },
+        { text: 'Při spuštění skriptu pozastavuje analýzu HTML („parsování“)' },
+      ],
+      ok: '',
+      link: 'https://www.w3schools.com/tags/att_script_defer.asp',
+    },
+    {
+      id: '30',
+      text: 'Co je kořenový prvek v HTML? ',
+      source: [
+        { text: '<!DOCTYPE html>' },
+        { text: '<html>' },
+        { text: '<root>' },
+        { text: '<body>' },
+      ],
+      ok: '',
+      link: 'https://www.w3.org/TR/2010/WD-html-markup-20100624/html.html',
+    },
+    {
+      id: '31',
+      text: 'Jáka sémantická značka se v HTML používá pro citování?',
+      source: [
+        { text: '<q>' },
+        { text: '<quote>' },
+        { text: '<blockquote>' },
+        { text: '<notation>' },
+      ],
+      ok: '',
+      link: 'https://www.w3schools.com/html/html_quotation_elements.asp',
+    },
+    {
+      id: '32',
+      text: 'Jak vypadají komentáře v HTML?',
+      source: [
+        { text: '//komentář' },
+        { text: '/* komentář */' },
+        { text: '<! komentář ->' },
+        { text: '<!-- komentář -->' },
+      ],
+      ok: '',
+      link: 'https://www.w3schools.com/html/html_comments.asp',
+    },
+    {
+      id: '33',
+      text: 'Jakým způsobem připojujeme soubory se styly (style.css) do HTML dokumentu?',
+      source: [
+        { text: '<style link="style.css">' },
+        { text: '<link rel="stylesheet" href="style.css">' },
+        { text: '<style src="style.css"></style>' },
+        { text: '<link style="style.css">' },
+      ],
+      ok: '',
+      link: 'https://www.w3schools.com/tags/att_link_rel.asp',
+    },
+    {
+      id: '34',
+      text: 'Které formáty obrázků zobrazuje většina webových prohlížečů?',
+      source: [
+        { text: 'JPG, GIF, TIF' },
+        { text: 'JPG, TIF, BMP' },
+        { text: 'TIF, BMP, GIF' },
+        { text: 'PNG, GIF, JPG' },
+      ],
+      ok: '',
+      link: 'https://www.vzhurudolu.cz/prirucka/obrazky-formaty',
+    },
+    {
+      id: '35',
+      text: 'Které formáty obrázků zobrazuje většina webJaký je správný kód odkazu, který po kliknutí odešle e-mail na e-mail@example.com s předmětem „Dobrý den“?',
+      source: [
+        { text: '<a href="mailto:email@example.com&subject=Hello">Click me</a>' },
+        { text: '<a href="mailto:email@example.com">Hello</a>' },
+        { text: '<a href="mailto:email@example.com?subject=Hello">Click me</a>' },
+        { text: '<a href="mailto:email@example.com?&subject=Hello">Click me</a>' },
+      ],
+      ok: '',
+      link: 'https://www.w3schools.com/tags/tag_address.asp',
+    },
+    {
+      id: '36',
+      text: 'Jakým atributem aktivujeme určitý prvek na stránce stisknutím určité klávesy?',
+      source: [
+        { text: 'accesskey' },
+        { text: 'shortcut' },
+        { text: 'keyboard' },
+        { text: 'access' },
+      ],
+      ok: '',
+      link: 'https://www.w3schools.com/tags/att_global_accesskey.asp',
+    },
+    {
+      id: '37',
+      text: 'Jak sémanticky nejlépe označit, že text odkazuje na vstup z klávesnice?',
+      source: [
+        { text: '<p>Zmáčkněte <tt>Enter</tt> a pokračujte.</p>' },
+        { text: '<p>Zmáčkněte <kbd>Enter</kbd> a pokračujte.</p>' },
+        { text: '<p>Zmáčkněte <samp>Enter</samp> a pokračujte.</p>' },
+        { text: '<p>K pokračování zmáčkněte Enter.</p>' },
+      ],
+      ok: '',
+      link: 'https://www.w3schools.com/tags/tag_kbd.asp',
+    },
+    {
+      id: '38',
+      text: 'Doplňte do mezery <link href="phone.css" rel="stylesheet" _____="print" />:',
+      source: [
+        { text: 'title' },
+        { text: 'type' },
+        { text: 'device' },
+        { text: 'media' },
+      ],
+      ok: '',
+      link: 'https://www.w3schools.com/tags/att_link_media.asp',
+    },
+    {
+      id: '39',
+      text: 'Chceme-li, aby se odkaz otevřel v novém okně, použijeme atribut:',
+      source: [
+        { text: '_blank' },
+        { text: '_self' },
+        { text: '_new' },
+        { text: '_parent' },
+      ],
+      ok: '',
+      link: 'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/rp',
+    },
+    {
+      id: '40',
+      text: 'Chceme-li, aby se odkaz otevřel v novém okně, použijeme atribut:',
+      source: [
+        { text: '_blank' },
+        { text: '_self' },
+        { text: '_new' },
+        { text: '_parent' },
+      ],
+      ok: '',
+      link: 'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/rp',
+    },
+    {
+      id: '41',
+      text: 'Které tři značky byly v HTML4 zastaralé, ale začal se zmovu používat v HTML5?',
+      source: [
+        { text: '<rb> <rp> <rt>nk' },
+        { text: '<acronym> <code> <wbr>' },
+        { text: '<hgroup> <q> <wbr>' },
+        { text: '<b> <i> <u>' },
+      ],
+      ok: '',
+      link: 'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/rp',
+    },
+    {
+      id: '42',
+      text: 'Doplňte do mezery <a href="https://es.yahoo.com/" hreflang="____" target="_blank">Visita Yahoo</a>:',
+      source: [
+        { text: 'es' },
+        { text: 'es-spanish' },
+        { text: 'es-es' },
+        { text: 'spanish' },
+      ],
+      ok: '',
+      link: 'https://www.w3schools.com/tags/ref_language_codes.asp',
+    },
+    {
+      id: '43',
+      text: 'Doplňte do mezery <form method="post" action="mailto:info@linkedin.com" ____="text/plain">',
+      source: [
+        { text: 'enctype' },
+        { text: 'media' },
+        { text: 'type' },
+        { text: 'rel' },
+      ],
+      ok: '',
+      link: 'https://www.w3schools.com/tags/ref_language_codes.asp',
+    },
+    // {
+    //   id: '44',
+    //   text: ' Doplňte do mezery v kódu: <section itemscope itemtype="http://schema.org/Restaurant">
+    //   <h1 itemprop="name">Nadia's Garden</h1>
+    //   <p itemscope ______ ______>
+    //     <span itemprop="ratingValue">4.5</span> stars - based on
+    //     <span itemprop="reviewCount">120</span> reviews
+    //   </p>
+    // </section> :',
+    //   source: [
+    //     { text: 'itemprop="aggregateRating" itemref="http://schema.org/AggregateRating"' },
+    //     { text: 'itemprop="aggregateRating" itemtype="http://schema.org/AggregateRating"' },
+    //     { text: 'itemid="aggregateRating" itemtype="http://schema.org/AggregateRating"' },
+    //     { text: 'itemid="aggregateRating" itemref="http://schema.org/AggregateRating"' },
+    //   ],
+    //   ok: '',
+    //   link: 'https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/itemtype',
+    // },
+    {
+      id: '45',
+      text: 'Který name atribut se nepoužívá uvnitř zančky <meta>?',
+      source: [
+        { text: 'charset' },
+        { text: 'viewport' },
+        { text: 'generator' },
+        { text: 'author' },
+      ],
+      ok: '',
+      link: 'https://www.w3schools.com/html/html_charset.asp',
+    },
+    {
+      id: '46',
+      text: 'Jaké je absolutní hodnota URL stránky s názvem page.html?',
+      source: [
+        { text: 'http://www.linkedin.com/dir/page.html' },
+        { text: 'page.html' },
+        { text: 'http://www.linkedin.com/page.html' },
+        { text: 'dir/page.html' },
+      ],
+      ok: '',
+      link: 'https://developer.mozilla.org/en-US/docs/Learn/Common_questions/What_is_a_URL',
+    },
+    {
+      id: '47',
+      text: 'Co má na starosti tento kód <audio controls src="sound.mp3" type="audio/mpeg">When does this text display?</audio>?',
+      source: [
+        { text: 'Text se zobrazí nad ovládacími prvky zvuku, pokud ho pomocí CSS neumístíme jinam.' },
+        { text: 'Text se zobrazí pod ovládacími prvky zvuku.' },
+        { text: 'Text se zobrazí, pokud prohlížeč nedokáže přehrát zvuk.' },
+        { text: 'Text se nikdy nezobrazí.' },
+      ],
+      ok: '',
+      link: 'https://www.w3schools.com/html/html5_audio.asp',
+    },
+
   ],
+  
 };
