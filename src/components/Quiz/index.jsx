6516a41d2ceca370b476e405ede340b8061d33ed
Lenka Silna <lenka.baricova@post.cz>
2021-06-05 15:44:33 +0200

    quiz list

--- src/components/Quiz/index.jsx
+++ src/components/Quiz/index.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import Question from '../Question';
 import { useParams } from 'react-router';
+import questions from './questions';
 
 const Quiz = () => {
   const { name } = useParams();
+  const questionForQuiz = questions[name];
+  const [selectedAnswers, setSelectedAnswers] = useState({});
+  const onSelect = (questionId, answerIndex) => {
+    console.log(questionId, answerIndex);
+  };
+
   return (
     <>
       <header className="heading">
         <h1 className="heading__text">Test {name.toUpperCase()}</h1>
       </header>
-      <Question />
+      {questionForQuiz.map((item, index) => (
+        <Question
+          key={item.id}
+          id={item.id}
+          heading={item.text}
+          answers={item.source}
+          link={item.link}
+          index={index + 1}
+          onSelect={onSelect}
+        />
+      ))}
     </>
   );
 };
