5428c4fb9d48e11c730f9fa29eabb1bfc3aff73b
Lenka Silna <lenka.baricova@post.cz>
2021-06-05 08:15:29 +0200

    Road map ready to data

--- src/components/RoadMap/content.js
+++ src/components/RoadMap/content.js
@@ -9,5 +9,21 @@ export const content = [
     name: 'CSS',
     text: '<li>CSS selektory nových vlastností CSS, jako je flex, grid, transform nebo gradienty</li><li>Preprocesory</li><li>variable fonts</li>Architektúru stylů (BEM aj.)</li><li>UI knihovny (Bootstrap, Material UI)</li><li>Lintovací nástroje</li><li>Custom properties (CSS variables)</li>',
   },
-  { id: 3, name: 'Javascript', text: '' },
+  {
+    id: 3,
+    name: 'Javascript',
+    text:
+      'Asynchronní JavaScript' +
+      'SPA (Single Page Applications)' +
+      'Webová API' +
+      'Frameworky' +
+      'Virtuální DOM' +
+      'Testování javascriptových aplikací (Karma, Mocha a Chai)' +
+      'lintovací nástroje (ESLint)',
+  },
+  { id: 4, name: 'Media', text: '' },
+  { id: 5, name: 'Design & UX', text: '' },
+  { id: 6, name: 'Best Practises', text: '' },
+  { id: 7, name: 'Nástroje', text: '' },
+  { id: 8, name: 'Soft skill', text: '' },
 ];
--- src/components/RoadMap/index.jsx
+++ src/components/RoadMap/index.jsx
@@ -2,16 +2,33 @@ import React from 'react';
 import './style.css';
 import Box from '../Box';
 import { content } from './content';
+import { Link } from 'react-router-dom';
 
 const RoadMap = () => (
-  <header className="heading">
-    <h1 className="heading__text">Road mapa webového vývoje</h1>
+  <>
+    <header className="heading">
+      <h1 className="heading__text">Road mapa webového vývoje</h1>
+    </header>
     <div className="roadMap__content">
       {content.map((item) => {
         return <Box key={item.id} name={item.name} text={item.text} />;
       })}
     </div>
-  </header>
+    <section className="quizRoadMap">
+      <div className="quizRoadMap__picture-wrapper">
+        <img
+          className="quizRoadMap__picture"
+          src="assets/quiz.svg"
+          alt="thinking lady"
+        />
+      </div>
+      <div className="quizRoadMap__text">
+        <Link to="/testy" className="quizRoadMap__link">
+          <p className="quizRoadMap__button">Otestuj se</p>
+        </Link>
+      </div>
+    </section>
+  </>
 );
 
 export default RoadMap;
