821b6a0d1b4177f1bded9b0c1ca41758e3a36713
Lenka Silna <lenka.baricova@post.cz>
2021-06-08 08:33:17 +0200

    data to education

--- src/components/Education/education.js
+++ src/components/Education/education.js
@@ -6,8 +6,44 @@ export const education = [
     link: 'https://www.youtube.com/',
     tips: [
       {
+        note: 'Frontendisti',
+        link: 'https://www.youtube.com/user/frontendisti',
+      },
+      {
         note: 'Yablko',
         link: 'https://www.youtube.com/channel/UC01guyOZpf40pUopBvdPwsg',
+      },
+      {
+        note: 'Hackni svou budoucnost',
+        link: 'https://www.youtube.com/channel/UCpUD5k7SOjsiPjlVV77TB7g',
+      },
+      {
+        note: 'DevelTalks',
+        link: 'https://www.youtube.com/channel/UCicItR-hrffb1oUK0Gmeeqg',
+      },
+      {
+        note: 'FrontEndFunn',
+        link: 'https://www.youtube.com/user/chandraja2010/featured',
+      },
+      {
+        note: 'freeCodeCamp',
+        link: 'https://www.youtube.com/channel/UC8butISFwT-Wl7EV0hUK0BQ',
+      },
+      {
+        note: 'Web Dev Simplified',
+        link: 'https://www.youtube.com/c/WebDevSimplified/featured',
+      },
+      {
+        note: 'Asociace UX',
+        link: 'https://www.youtube.com/channel/UChUmRx9HfIvXUtib7EFKn8g/featured',
+      },
+      {
+        note: 'Design Xstream',
+        link: 'https://www.youtube.com/c/DesignXStream/videos',
+      },
+      {
+        note: 'DevTips',
+        link: 'https://www.youtube.com/c/DevTipsForDesigners/featured',
       },
     ],
   },
@@ -16,7 +52,12 @@ export const education = [
     name: 'Czechitas',
     text: '',
     link: 'https://www.czechitas.cz/',
-    tips: [],
+    tips: [
+      {
+        note: 'Digitální akademie',
+        link: 'https://www.czechitas.cz/tema/digitalni-akademie',
+      },
+    ],
   },
   {
     id: 3,
@@ -30,6 +71,11 @@ export const education = [
     name: 'Frontend Mentor',
     text: 'Projekty na procvičení s komunitou, která vám poskytne radu i zpětnou vazbu.',
     link: 'https://www.frontendmentor.io/',
-    tips: [],
+    tips: [
+      {
+        note: 'Facebooková skupina Holky z IT mají Slack kanál kde si radí s projekty z FM',
+        link: 'https://www.facebook.com/groups/holkyzit/permalink/1563238557207182',
+      },
+    ],
   },
 ];
